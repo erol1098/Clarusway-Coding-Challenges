6824d3f4bef0089d64025bbfc30d720d3e6b29d7
erol1098 <erol1098@gmail.com>
2022-05-13 14:40:40 +0300

    Corrections Made.

--- Javascript/CC-01/app.js
+++ Javascript/CC-01/app.js
@@ -1,5 +1,6 @@
 "use strict";
 const word = document.querySelector("#word");
+const warn = document.querySelector(".main__entry-warning");
 const inputSection = document.querySelector(".main__input");
 const inputWord = document.querySelector(".main__input--input");
 const answerSection = document.querySelector(".main__answer");
@@ -7,21 +8,30 @@ const reversed = document.querySelector(".main__answer--reversed");
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", (e) => {
-  if (word.value) {
-    const input = word.value;
-    let output = "";
+  let input = word.value.trim().replace(/[.?!]/g);
+  input = input.split(" ");
+  if (word.value && input.length > 1) {
+    let output = [];
     for (let i = input.length - 1; i >= 0; i--) {
-      output += input[i];
+      output.push(input[i]);
     }
     inputSection.classList.remove("hidden");
     answerSection.classList.remove("hidden");
-    inputWord.textContent = input.toLowerCase();
-    reversed.textContent = output.toLowerCase();
+    inputWord.textContent = input.join(" ");
+    reversed.textContent = output.join(" ");
     word.value = "";
+  } else if (!word.value.trim()) {
+    warn.textContent = "🛑Nothing entered!🛑";
+    warn.classList.remove("hidden");
+  } else if (input.length === 1) {
+    warn.textContent = "🛑Please enter more than one word!🛑";
+    warn.classList.remove("hidden");
   }
 });
 
 word.addEventListener("focus", (e) => {
   inputSection.classList.add("hidden");
   answerSection.classList.add("hidden");
+  warn.classList.add("hidden");
+  word.value = "";
 });
